1e135ad49e1d045981a00df5440aa54ceb71b515
7f <git7f@yandex.com>
2017-04-10 18:02:33 +0800

    add auto update script

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd $DIR
+
+curl -o oid.htm http://www.saskatchewan.ca/residents/moving-to-saskatchewan/immigrating-to-saskatchewan/saskatchewan-immigrant-nominee-program/applicants-international-skilled-workers/international-skilled-worker-occupations-in-demand
+
+curl -o max.htm http://www.saskatchewan.ca/residents/moving-to-saskatchewan/immigrating-to-saskatchewan/saskatchewan-immigrant-nominee-program/maximum-number-of-sinp-applications
+
+git add .
+git commit -m "auto update"
+git push
+
+popd
